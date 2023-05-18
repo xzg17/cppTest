725257dcfb71929e2b5c17f49860623d1bc3548d
xzg17 <128256034+xzg17@users.noreply.github.com>
2023-05-18 20:33:15 +0900

    Update Sample.cpp

--- src/Sample.cpp
+++ src/Sample.cpp
@@ -7,14 +7,46 @@ typedef struct {
 } CustomObject;
 
 static PyTypeObject CustomType = {
-    PyVarObject_HEAD_INIT(NULL, 0)
-    .tp_name = "custom.Custom",
-    .tp_doc = PyDoc_STR("Custom objects"),
-    .tp_basicsize = sizeof(CustomObject),
-    .tp_itemsize = 0,
-    .tp_flags = Py_TPFLAGS_DEFAULT,
-    .tp_new = PyType_GenericNew,
+    PyVarObject_HEAD_INIT(NULL, 0) "custom.Custom", /* tp_name */
+    sizeof(CustomObject),                           /* tp_basicsize */
+    0,                                              /* tp_itemsize */
+    /* methods */
+    0, /* tp_dealloc */
+    0,                        /* tp_print */
+    0,                        /* tp_getattr */
+    0,                        /* tp_setattr */
+    0,                        /* tp_reserved */
+    0,                        /* tp_repr */
+    0,                        /* tp_as_number */
+    0,                        /* tp_as_sequence */
+    0,                        /* tp_as_mapping */
+    0,                        /* tp_hash */
+    0,                        /* tp_call */
+    (reprfunc)Line_str,       /* tp_str */
+    0,                        /* tp_getattro */
+    0,                        /* tp_setattro */
+    0,                        /* tp_as_buffer */
+    Py_TPFLAGS_DEFAULT,       /* tp_flags */
+    PyDoc_STR("Custom objects"),/* tp_doc */
+    0,                        /* tp_traverse */
+    0,                        /* tp_clear */
+    0,                        /* tp_richcompare */
+    0,                        /* tp_weaklistoffset */
+    0,                        /* tp_iter */
+    0,                        /* tp_iternext */
+    0,                        /* tp_methods */
+    0,                        /* tp_members */
+    0,                        /* tp_getset */
+    0,                        /* tp_base */
+    0,                        /* tp_dict */
+    0,                        /* tp_descr_get */
+    0,                        /* tp_descr_set */
+    0,                        /* tp_dictoffset */
+    0,                        /* tp_init */
+    0,                        /* tp_alloc */
+    PyType_GenericNew,        /* tp_new */
 };
+
 
 static PyModuleDef custommodule = {
     PyModuleDef_HEAD_INIT,
