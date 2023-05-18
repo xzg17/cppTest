4fa00480c92cffca06712e9b1b92973c395aa1a2
xzg17 <128256034+xzg17@users.noreply.github.com>
2023-05-19 01:46:42 +0900

    Update Sample.cpp

--- src/Sample.cpp
+++ src/Sample.cpp
@@ -69,7 +69,7 @@ static int *Dummy_init(CustomObject *self, PyObject *args){
     int a, b;
     
     if(PyArg_ParseTuple(args, "ii", &a, &b)){
-        self->myclass = new MyClass(a, b);
+        self->myclass = new MyClass(0, 0);
     } else {
         PyErr_SetString(PyExc_ValueError, "Invalid arguments. Use {a, b}.\n for example: MyClass(10, 20)");
         self->myclass = new MyClass(0, 0);
