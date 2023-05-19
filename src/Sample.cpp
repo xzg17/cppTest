514c4a1fe138290661e4811849144c0760745e72
xzg17 <128256034+xzg17@users.noreply.github.com>
2023-05-19 13:50:36 +0900

    Update Sample.cpp

--- src/Sample.cpp
+++ src/Sample.cpp
@@ -67,7 +67,7 @@ static PyObject *Class_sum(CustomObject *self){
 static int *Dummy_init(CustomObject *self, PyObject *args){
     int a = 0;
     int b = 0;
-    if (PyArg_ParseTuple(args, "|ii", &a, &b)) {
+    if (PyArg_ParseTuple(args, "ii", &a, &b)) {
         self->myclass = new MyClass(a, b);        
     } else {
         PyErr_SetObject(PyExc_ValueError, args);
