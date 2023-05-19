7581dd4758562fa2d005fca3648829761b2891bb
xzg17 <128256034+xzg17@users.noreply.github.com>
2023-05-19 14:17:09 +0900

    Update Sample.cpp

--- src/Sample.cpp
+++ src/Sample.cpp
@@ -65,6 +65,9 @@ static PyObject *Class_sum(CustomObject *self){
 };
 
 static int *Dummy_init(CustomObject *self, PyObject *args){
+    if(args==Py_None){
+        PyErr_SetString(PyExc_ValueError, "Dummy!");
+    }
     int a = 0;
     int b = 0;
     if (PyArg_ParseTuple(args, "|ii", &a, &b)) {
