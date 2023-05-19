364b7ca25510305ef09d997f3242c7d6c4312aba
xzg17 <128256034+xzg17@users.noreply.github.com>
2023-05-19 14:39:56 +0900

    Update Sample.cpp

--- src/Sample.cpp
+++ src/Sample.cpp
@@ -66,7 +66,9 @@ static PyObject *Class_sum(CustomObject *self){
 
 static int *Dummy_init(CustomObject *self, PyObject *args){
     //my memo:引数なしでPyType_Check(args)はnon_zero
-    //*
+    //my memo:引数なしでPyType_CheckExact(args)でもnon_zero
+    //args==NULL,args==Py_Noneはうまくいかない。これどうするの？
+    /*
     if(!PyType_CheckExact(args)){
         PyErr_SetString(PyExc_ValueError, "Dummy!");
     }//*/
