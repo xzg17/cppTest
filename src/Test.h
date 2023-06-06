599c4fe6930d611fe19951475f01626418ddae7f
xzg17 <128256034+xzg17@users.noreply.github.com>
2023-06-06 09:34:19 +0900

    Update Test.h

--- src/Test.h
+++ src/Test.h
@@ -3,21 +3,18 @@ typedef struct{
     int f;//from
     int t;//to
     int p;//piece
-    int pro;//progress
-    int d;//dir
+    int g;//get
 } move;
 
 class TestBoard{
 public:
-    TestBoard(int bo[25], int il[2]);
+    TestBoard(int bo[13], int hands[6]);
     int push(move mo,);
 private:
     int board[25];
     int ills[2];
 };
-TestBoard::TestBoard(int bo[25], int il[2]){
-    this->ills[0] = il[0];
-    this->ills[1] = il[1];
+TestBoard(int bo[13], int hands[6]){
     this->board[0] = bo[0];
     this->board[1] = bo[1];
     this->board[2] = bo[2];
@@ -31,16 +28,4 @@ TestBoard::TestBoard(int bo[25], int il[2]){
     this->board[10] = bo[10];
     this->board[11] = bo[11];
     this->board[12] = bo[12];
-    this->board[13] = bo[13];
-    this->board[14] = bo[14];
-    this->board[15] = bo[15];
-    this->board[16] = bo[16];
-    this->board[17] = bo[17];
-    this->board[18] = bo[18];
-    this->board[19] = bo[19];
-    this->board[20] = bo[20];
-    this->board[21] = bo[21];
-    this->board[22] = bo[22];
-    this->board[23] = bo[23];
-    this->board[24] = bo[24];
 };
