d943d426d14710aa6f6c9d5c9560ab52d8885d18
xzg17 <128256034+xzg17@users.noreply.github.com>
2023-06-06 18:28:46 +0900

    Update Test.cpp

--- src/Test.cpp
+++ src/Test.cpp
@@ -109,67 +109,7 @@ PyInit_customZ(void){
 
     return m;
 }
-
-#define PY_SSIZE_T_CLEAN
-#include <Python.h>
-#include "Test.h"
-//0,1,2,3,4,5<->null,lion,jiraffe,elephant,chick,hen
-typedef struct {
-    PyObject_HEAD
-    /* Type-specific fields go here. */
-    TestBoard *test_bo
-} Py_Class_Board;
-
-static PyObject *Board_moves(Py_Class_Board *self);
-static int *Dummy_init(Py_Class_Board *self, PyObject *args);
-
-static PyMethodDef Py_Class_Board_methods[] = {
-    {"moves", (PyCFunction)Board_moves, METH_VARARGS, "generate moves."},
-    {NULL} /* Sentinel */
-};
-
-
-static PyTypeObject ClassType = {
-    PyVarObject_HEAD_INIT(NULL, 0) "Test", /* tp_name */
-    sizeof(Py_Class_Board),                           /* tp_basicsize */
-    0,                                              /* tp_itemsize */
-    /* methods */
-    0, /* tp_dealloc */
-    0,                        /* tp_print */
-    0,                        /* tp_getattr */
-    0,                        /* tp_setattr */
-    0,                        /* tp_reserved */
-    0,                        /* tp_repr */
-    0,                        /* tp_as_number */
-    0,                        /* tp_as_sequence */
-    0,                        /* tp_as_mapping */
-    0,                        /* tp_hash */
-    0,                        /* tp_call */
-    0,       /* tp_str */
-    0,                        /* tp_getattro */
-    0,                        /* tp_setattro */
-    0,                        /* tp_as_buffer */
-    Py_TPFLAGS_DEFAULT,       /* tp_flags */
-    PyDoc_STR("Class objects"),/* tp_doc */
-    0,                        /* tp_traverse */
-    0,                        /* tp_clear */
-    0,                        /* tp_richcompare */
-    0,                        /* tp_weaklistoffset */
-    0,                        /* tp_iter */
-    0,                        /* tp_iternext */
-    Py_Class_Board_methods,   /* tp_methods */
-    0,                        /* tp_members */
-    0,                        /* tp_getset */
-    0,                        /* tp_base */
-    0,                        /* tp_dict */
-    0,                        /* tp_descr_get */
-    0,                        /* tp_descr_set */
-    0,                        /* tp_dictoffset */
-    (initproc)Dummy_init,     /* tp_init */
-    0,                        /* tp_alloc */
-    PyType_GenericNew,        /* tp_new */
-};
-//*
+/*
 static PyObject *Board_moves(Py_Class_Board *self){
     int moves[48];
     int i,j,m;
