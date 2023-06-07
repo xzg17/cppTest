55fe47cec683254cf19d162d755e0a9c8561985a
xzg17 <128256034+xzg17@users.noreply.github.com>
2023-06-07 20:06:00 +0900

    Create MoveStack.hpp

---
+++ src/MoveStack.hpp
@@ -0,0 +1,24 @@
+class MoveStack{
+private:
+    int top;
+    int stack[200];
+public:
+    void MoveStack();
+    void push(int x);
+    int pop();
+    
+}
+void MoveStack::MoveStack(){
+    this->top=-1;
+};
+
+void MoveStack::push(int x){
+    this->top++;
+    this->stack[this->top]=x;
+};
+
+int MoveStack::pop(){
+    int x = this->stack[this->top];
+    this->top--;
+    return x;
+};
