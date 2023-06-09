b789f215ab9596969b0172e4905ec9c266df866a
xzg17 <128256034+xzg17@users.noreply.github.com>
2023-06-09 13:16:45 +0900

    Update and rename DID.h to Board.h

---
+++ src/Board.h
@@ -0,0 +1,83 @@
+#include <string>
+#include "Piece.hpp"
+
+class Board{
+public:
+    TestBoard(int bo[14]);
+    void rotate();
+    void push(int move);
+    int next_player();
+    int is_end();
+    std::string to_string();
+    int board[14];
+    int next;
+    int end;//0,1
+private:
+};
+TestBoard::TestBoard(int bo[14]){
+    this->board[0] = bo[0];
+    this->board[1] = bo[1];
+    this->board[2] = bo[2];
+    this->board[3] = bo[3];
+    this->board[4] = bo[4];
+    this->board[5] = bo[5];
+    this->board[6] = bo[6];
+    this->board[7] = bo[7];
+    this->board[8] = bo[8];
+    this->board[9] = bo[9];
+    this->board[10] = bo[10];
+    this->board[11] = bo[11];
+    this->board[12] = bo[12];
+    this->board[13] = bo[13];
+    this->next=0;
+    this->end=0;
+};
+void TestBoard::rotate(){
+    int _;
+    _ = this->board[0];
+    this->board[0] = -this->board[13];
+    this->board[13] = -_;
+    _ = this->board[1];
+    this->board[1] = -this->board[12];
+    this->board[12] = -_;
+    _ = this->board[2];
+    this->board[2] = -this->board[11];
+    this->board[11] = -_;
+    _ = this->board[3];
+    this->board[3] = -this->board[10];
+    this->board[10] = -_;
+    _ = this->board[4];
+    this->board[4] = -this->board[9];
+    this->board[9] = -_;
+    _ = this->board[5];
+    this->board[5] = -this->board[8];
+    this->board[8] = -_;
+    _ = this->board[6];
+    this->board[6] = -this->board[7];
+    this->board[7] = -_;
+};
+void TestBoard::push(int move){
+    int f,t;
+    this->history[this->tesu]=move;
+    f=move / 14;
+    t=move % 14;
+    this->board[t]=this->board[f];
+    this->board[f]=0;
+    this->next=1-next;
+    this->rotate();
+};
+int TestBoard::is_end(){
+    return this->end;
+};
+
+int TestBoard::next_player(){
+    return this->next;
+};
+
+std::string TestBoard::to_string(){
+    return 
+        " "+std::to_string(this->board[1])+" "+std::to_string(this->board[2])+" "+std::to_string(this->board[3])+"\n"+
+        " "+std::to_string(this->board[4])+" "+std::to_string(this->board[5])+" "+std::to_string(this->board[6])+"\n"+
+        " "+std::to_string(this->board[7])+" "+std::to_string(this->board[8])+" "+std::to_string(this->board[9])+"\n"+
+        " "+std::to_string(this->board[10])+" "+std::to_string(this->board[11])+" "+std::to_string(this->board[12])+"\n";
+}
