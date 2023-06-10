83d8bbe6c85abae5d91cf615fb4f573c4cc0e16a
xzg17 <128256034+xzg17@users.noreply.github.com>
2023-06-11 03:14:42 +0900

    Update Board.cpp

--- src/Board.cpp
+++ src/Board.cpp
@@ -37,6 +37,7 @@ static PyObject *my_debug1(Py_Class_Board *self){
 static PyMethodDef Py_Class_Board_methods[] = {
     {"moves", (PyCFunction)Board_moves, METH_VARARGS, "(ToT)"},
     {"catch", (PyCFunction)catch_moves, METH_VARARGS, "(^o^)v"},
+    {"tesu", (PyCFunction)get_tesu, METH_VARARGS, "(^o^)v"},
     {"catch_or_moves", (PyCFunction)catch_or_moves, METH_VARARGS, "(ToT)/\(^o^)v"},
     {"rotate", (PyCFunction)rotate_board, METH_VARARGS, "(^q^)"},
     {"push", (PyCFunction)push_move, METH_VARARGS, "(O_O)"},
