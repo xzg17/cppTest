b62893e1db85b2939e288f1b1aabe919b3cf8ebf
xzg17 <128256034+xzg17@users.noreply.github.com>
2023-08-04 02:49:12 +0900

    Creating TsuiBoard5.hpp now....

---
+++ src/TsuiBoard5.hpp
@@ -0,0 +1,99 @@
+/*
+空:0
+歩:1
+銀:2
+角:3
+飛:4
+金:5
+玉:6
+合法手生成はひらがなすいしょうとかを参考にした
+*/
+class TsuiBoard5{
+public:
+    Board();
+    void rotate();
+    void push(int move);
+    Board pushed(int move);
+    int next_player();
+    int is_end();
+    std::string to_string();
+    int board[14];
+    int moves1[200];//成でない「指す」手。移動先*方向=25*8=200
+    int moves2[25];//可成域への成る手。移動先*方向=5*5=25
+    int moves3[60];//非可成域への成る手。移動先*方向=20*3=60
+    int moves4[25];//打つ手。移動先*方向=25*1=25
+    int next;
+    int tesu;
+    int end;//0,1
+private:
+};
+Board::Board(){
+    this->board[0] = bo[0];
+    this->board[1] = bo[1];
+    this->board[2] = bo[2];
+    this->board[3] = bo[3];
+    this->board[4] = bo[4];
+    this->board[5] = bo[5];
+    this->board[6] = bo[6];
+    this->board[7] = bo[7];
+    this->board[8] = bo[8];
+    this->board[9] = bo[9];
+    this->board[10] = bo[10];
+    this->board[11] = bo[11];
+    this->board[12] = bo[12];
+    this->board[13] = bo[13];
+    this->next=turn;
+    this->end=0;
+    this->tesu=0;
+};
+void Board::rotate(){
+    int _;
+    _ = this->board[0];
+    this->board[0] = -this->board[13];
+    this->board[13] = -_;
+    _ = this->board[1];
+    this->board[1] = -this->board[12];
+    this->board[12] = -_;
+    _ = this->board[2];
+    this->board[2] = -this->board[11];
+    this->board[11] = -_;
+    _ = this->board[3];
+    this->board[3] = -this->board[10];
+    this->board[10] = -_;
+    _ = this->board[4];
+    this->board[4] = -this->board[9];
+    this->board[9] = -_;
+    _ = this->board[5];
+    this->board[5] = -this->board[8];
+    this->board[8] = -_;
+    _ = this->board[6];
+    this->board[6] = -this->board[7];
+    this->board[7] = -_;
+};
+void Board::push(int move){
+    int f,t;
+    f=move / 14;
+    t=move % 14;
+    if(this->board[t]==-1){
+        this->end=1;
+    }
+    this->board[t]=this->board[f];
+    this->board[f]=0;
+    this->next=1-next;
+    this->tesu+=1;
+    this->rotate();
+};
+
+Board Board::pushed(int move){
+    Board board(this->board, this->next);
+    board.push(move);
+    return board;
+};
+
+int Board::is_end(){
+    return this->end;
+};
+
+int Board::next_player(){
+    return this->next;
+};
