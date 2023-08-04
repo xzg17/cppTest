f0448e2a412f046541316fe0cd3121a6f592fc44
xzg17 <128256034+xzg17@users.noreply.github.com>
2023-08-04 09:30:14 +0900

    Creating TsuiBoard5.hpp now...

--- src/TsuiBoard5.hpp
+++ src/TsuiBoard5.hpp
@@ -6,10 +6,15 @@
 飛:4
 金:5
 玉:6
+成:+6
+偶:11
 合法手生成はひらがなすいしょうとかを参考にした
 */
 //マジックナンバー？#define？なにそれおいしいの？
 //可読性？酸っぱそうな名前だね
+//viewerでプレイする場合打歩詰反則に対応できないが同実装するか
+//lishogiで再生することを前提としよう
+//よく考えたらrotateするから純粋な再生は無理かも
 class TsuiBoard5{
 public:
     Board();
@@ -20,8 +25,9 @@ public:
     int is_close_check();//隣からの王手判定
     int is_end();
     std::string to_string();
-    int board[38];//0~24盤25~34持駒35,36反則37手数
+    int board[62];//0~48盤49~58持駒59,60反則61手数
     int kpos[2];
+    int end;
     //以下使うか怪しいもの
     int moves1[200];//成でない「指す」手。移動先*方向=25*8=200
     int moves2[25];//可成域への成る手。移動先*方向=5*5=25(+200)
@@ -30,105 +36,168 @@ public:
 private:
 };
 Board::Board(){
-    this->board[0] = -4;
-    this->board[1] = -3;
-    this->board[2] = -2;
-    this->board[3] = -5;
-    this->board[4] = -6;
-    this->board[5] = 0;
-    this->board[6] = 0;
-    this->board[7] = 0;
-    this->board[8] = 0;
-    this->board[9] = -1;
-    this->board[10] = 0;
-    this->board[11] = 0;
-    this->board[12] = 0;
-    this->board[13] = 0;
-    this->board[14] = 0;
-    this->board[15] = 1;
+    this->board[0] = 11;
+    this->board[1] = 11;
+    this->board[2] = 11;
+    this->board[3] = 11;
+    this->board[4] = 11;
+    this->board[5] = 11;
+    this->board[6] = 11;
+    this->board[7] = 11;
+    this->board[8] = -4;
+    this->board[9] = -3;
+    this->board[10] = -2;
+    this->board[11] = -5;
+    this->board[12] = -6;
+    this->board[13] = 11;
+    this->board[14] = 11;
+    this->board[15] = 0;
     this->board[16] = 0;
     this->board[17] = 0;
     this->board[18] = 0;
-    this->board[19] = 0;
-    this->board[20] = 6;
-    this->board[21] = 5;
-    this->board[22] = 2;
-    this->board[23] = 3;
-    this->board[24] = 4;
+    this->board[19] = -1;
+    this->board[20] = 11;
+    this->board[21] = 11;
+    this->board[22] = 0;
+    this->board[23] = 0;
+    this->board[24] = 0;
     this->board[25] = 0;
     this->board[26] = 0;
-    this->board[27] = 0;
-    this->board[28] = 0;
-    this->board[29] = 0;
+    this->board[27] = 11;
+    this->board[28] = 11;
+    this->board[29] = 1;
     this->board[30] = 0;
     this->board[31] = 0;
     this->board[32] = 0;
     this->board[33] = 0;
-    this->board[34] = 0;
-    this->board[35] = 9;
-    this->board[36] = 9;
-    this->board[37] = 0;
-    this->kpos[0] = 20;
-    this->kpos[1] = 4;
+    this->board[34] = 11;
+    this->board[35] = 11;
+    this->board[36] = 6;
+    this->board[37] = 5;
+    this->board[38] = 2;
+    this->board[39] = 3;
+    this->board[40] = 4;
+    this->board[41] = 11;
+    this->board[42] = 11;
+    this->board[43] = 11;
+    this->board[44] = 11;
+    this->board[45] = 11;
+    this->board[46] = 11;
+    this->board[47] = 11;
+    this->board[48] = 11;
+    this->board[49] = 0;
+    this->board[50] = 0;
+    this->board[51] = 0;
+    this->board[52] = 0;
+    this->board[53] = 0;
+    this->board[54] = 0;
+    this->board[55] = 0;
+    this->board[56] = 0;
+    this->board[57] = 0;
+    this->board[58] = 0;
+    this->board[59] = 9;
+    this->board[60] = 9;
+    this->board[61] = 0;
+
+    this->kpos[0] = 36;
+    this->kpos[1] = 12;
 };
 void Board::rotate(){
     int _;
     _ = this->board[0];
-    this->board[0] = -this->board[24];
-    this->board[24] = -_;
+    this->board[0] = -this->board[48];
+    this->board[48] = -_;
     _ = this->board[1];
-    this->board[1] = -this->board[23];
-    this->board[23] = -_;
+    this->board[1] = -this->board[47];
+    this->board[47] = -_;
     _ = this->board[2];
-    this->board[2] = -this->board[22];
-    this->board[22] = -_;
+    this->board[2] = -this->board[46];
+    this->board[46] = -_;
     _ = this->board[3];
-    this->board[3] = -this->board[21];
-    this->board[21] = -_;
+    this->board[3] = -this->board[45];
+    this->board[45] = -_;
     _ = this->board[4];
-    this->board[4] = -this->board[20];
-    this->board[20] = -_;
+    this->board[4] = -this->board[44];
+    this->board[44] = -_;
     _ = this->board[5];
-    this->board[5] = -this->board[19];
-    this->board[19] = -_;
+    this->board[5] = -this->board[43];
+    this->board[43] = -_;
     _ = this->board[6];
-    this->board[6] = -this->board[18];
-    this->board[18] = -_;
+    this->board[6] = -this->board[42];
+    this->board[42] = -_;
     _ = this->board[7];
-    this->board[7] = -this->board[17];
-    this->board[17] = -_;
+    this->board[7] = -this->board[41];
+    this->board[41] = -_;
     _ = this->board[8];
-    this->board[8] = -this->board[16];
-    this->board[16] = -_;
+    this->board[8] = -this->board[40];
+    this->board[40] = -_;
     _ = this->board[9];
-    this->board[9] = -this->board[15];
-    this->board[15] = -_;
+    this->board[9] = -this->board[39];
+    this->board[39] = -_;
     _ = this->board[10];
-    this->board[10] = -this->board[14];
-    this->board[14] = -_;
+    this->board[10] = -this->board[38];
+    this->board[38] = -_;
     _ = this->board[11];
-    this->board[11] = -this->board[13];
-    this->board[13] = -_;
-    this->board[12] = -this->board[12];
+    this->board[11] = -this->board[37];
+    this->board[37] = -_;
+    _ = this->board[12];
+    this->board[12] = -this->board[36];
+    this->board[36] = -_;
+    _ = this->board[13];
+    this->board[13] = -this->board[35];
+    this->board[35] = -_;
+    _ = this->board[14];
+    this->board[14] = -this->board[34];
+    this->board[34] = -_;
+    _ = this->board[15];
+    this->board[15] = -this->board[33];
+    this->board[33] = -_;
+    _ = this->board[16];
+    this->board[16] = -this->board[32];
+    this->board[32] = -_;
+    _ = this->board[17];
+    this->board[17] = -this->board[31];
+    this->board[31] = -_;
+    _ = this->board[18];
+    this->board[18] = -this->board[30];
+    this->board[30] = -_;
+    _ = this->board[19];
+    this->board[19] = -this->board[29];
+    this->board[29] = -_;
+    _ = this->board[20];
+    this->board[20] = -this->board[28];
+    this->board[28] = -_;
+    _ = this->board[21];
+    this->board[21] = -this->board[27];
+    this->board[27] = -_;
+    _ = this->board[22];
+    this->board[22] = -this->board[26];
+    this->board[26] = -_;
+    _ = this->board[23];
+    this->board[23] = -this->board[25];
+    this->board[25] = -_;
+    this->board[24] = -this->board[24];
     
-    _ = this->board[25];
-    this->board[25] = this->board[30];
-    this->board[30] = _;
-    _ = this->board[26];
-    this->board[26] = this->board[31];
-    this->board[31] = _;
-    _ = this->board[27];
-    this->board[27] = this->board[32];
-    this->board[32] = _;
-    _ = this->board[28];
-    this->board[28] = this->board[33];
-    this->board[33] = _;
-    _ = this->board[29];
-    this->board[29] = this->board[34];
-    this->board[34] = _;
-    _ = this->board[35];
-    this->board[35] = this->board[36];
-    this->board[36] = _;
+    _ = this->board[49];
+    this->board[49] = this->board[54];
+    this->board[54] = _;
+    _ = this->board[50];
+    this->board[50] = this->board[55];
+    this->board[55] = _;
+    _ = this->board[51];
+    this->board[51] = this->board[56];
+    this->board[56] = _;
+    _ = this->board[52];
+    this->board[52] = this->board[57];
+    this->board[57] = _;
+    _ = this->board[53];
+    this->board[53] = this->board[58];
+    this->board[58] = _;
+    
+    _ = this->board[59];
+    this->board[59] = this->board[60];
+    this->board[60] = _;
+    
     _ = this->kpos[0];
     this->kpos[0] = this->kpos[1];
     this->kpos[1] = _;
