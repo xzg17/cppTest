0e14f78bf20d8cf9e2c46fecbbc52ac267eb76c7
xzg17 <128256034+xzg17@users.noreply.github.com>
2023-08-06 05:19:08 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ src_dir = 'src'
 
 #ext_modules = [Extension('customZ',[src_dir + '/Sample.cpp'])]
 #ext_modules = [Extension('customZ',[src_dir + '/Test.cpp'])]
-ext_modules = [Extension('customZ',[src_dir + '/Test.cpp']),Extension('np_nd_DIDS',[src_dir + '/Board.cpp'])]
+ext_modules = [Extension('customZ',[src_dir + '/Test.cpp']),Extension('np_nd_DIDS',[src_dir + '/Board.cpp']),Extension('MiniTsuitate',[src_dir + '/TsuiBoard5.cpp'])]
 setup(
   name='custom',
   ext_modules = ext_modules,
