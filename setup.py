887094008c413ab9948289fc62261d0dafae5c20
xzg17 <128256034+xzg17@users.noreply.github.com>
2023-08-06 11:02:02 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ src_dir = 'src'
 
 #ext_modules = [Extension('customZ',[src_dir + '/Sample.cpp'])]
 #ext_modules = [Extension('customZ',[src_dir + '/Test.cpp'])]
-ext_modules = [Extension('customZ',[src_dir + '/Test.cpp']),Extension('np_nd_DIDS',[src_dir + '/Board.cpp']),Extension('Tsuitate55',[src_dir + '/TsuiBoard5.cpp'])]
+ext_modules = [Extension('customZ',[src_dir + '/Test.cpp']),Extension('np_nd_DIDS',[src_dir + '/Board.cpp'])]
 setup(
   name='custom',
   ext_modules = ext_modules,
