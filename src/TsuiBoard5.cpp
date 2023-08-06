74b6b2c00b5d57d3eb7a7d653b0e0fef8878c36b
xzg17 <128256034+xzg17@users.noreply.github.com>
2023-08-07 05:45:00 +0900

    Update TsuiBoard5.cpp

--- src/TsuiBoard5.cpp
+++ src/TsuiBoard5.cpp
@@ -149,7 +149,6 @@ static PyObject *board_moves(Py_Class_TsuiBoard *self){
     self->board2->pseudo_moves2(moves2);
     self->board2->pseudo_moves3(moves3);
     self->board2->pseudo_moves4(moves4);
-    /*
     for(int i = 0;i < 200;i++){
         if(moves1[i]){
             PyObject *tuple = PyTuple_New(2);
@@ -162,6 +161,7 @@ static PyObject *board_moves(Py_Class_TsuiBoard *self){
             };
         };
     };
+    /*
     for(int i = 0;i < 25;i++){
         if(moves2[i]){
             PyObject *tuple = PyTuple_New(2);
