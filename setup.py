77e66db71a4d85179a30500d028d89f67a497a42
xzg17 <128256034+xzg17@users.noreply.github.com>
2023-08-09 19:27:23 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ src_dir = 'src'
 #ext_modules = [Extension('customZ',[src_dir + '/Sample.cpp'])]
 ext_modules = [Extension('customZ',[src_dir + '/Test.cpp'])]
 ext_modules.append(Extension('np_nd_DIDS',[src_dir + '/Board.cpp']))
-//ext_modules.append(Extension('BlindMiniShogi',[src_dir + '/TsuiBoard5.cpp']))
+#ext_modules.append(Extension('BlindMiniShogi',[src_dir + '/TsuiBoard5.cpp']))
 setup(
   name='custom',
   ext_modules = ext_modules,
