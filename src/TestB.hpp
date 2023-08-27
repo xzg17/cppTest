4d48628bb288fa74f0f2433d1756af462fc9c839
xzg17 <128256034+xzg17@users.noreply.github.com>
2023-08-27 10:35:00 +0900

    Update TestB.hpp

--- src/TestB.hpp
+++ src/TestB.hpp
@@ -1,7 +1,11 @@
 class Test_Class{
 public:
     int num;
+    Test_Class();
     Test_Class(int num);
+};
+Test_Class::Test_Class(){
+    this->num = 0;
 };
 Test_Class::Test_Class(int num){
     this->num = num;
